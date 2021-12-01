ac70eed1474b4cf0fe2c291856a1c64f7aee722f
kengha0 <azizkaharov1@gmail.com>
2021-12-01 09:55:51 +0600

    first

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function(){
+    $(".carousel").owlCarousel({
+        items: 5,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 1000,
+        autoplayHoverPause: true,
+
+    });
+
+
+});
